c37a3e2f471454b61e7dee430d5e4855a6a41a8a
Mursalat Asyidiq <mursalat.asyidiq@spesolution.com>
2024-08-26 21:46:48 +0700

    feat: add status type

--- Modules/Quotation/Resources/views/create.blade.php
+++ Modules/Quotation/Resources/views/create.blade.php
@@ -64,6 +64,10 @@
                                         <label for="status">Status <span class="text-danger">*</span></label>
                                         <select class="form-control" name="status" id="status" required>
                                             <option value="Pending">Pending</option>
+                                            <option value="Menunggu Pembayaran">Menunggu Pembayaran</option>
+                                            <option value="Pembayaran Dikonfirmasi">Pembayaran Dikonfirmasi</option>
+                                            <option value="Diproses">Diproses</option>
+                                            <option value="Selesai">Selesai</option>
                                             <option value="Sent">Terkirim</option>
                                         </select>
                                     </div>
